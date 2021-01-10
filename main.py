187f7dea1316cf3ca8918e2ecc6957d9470bda96
llllcccjjj <70000656+llllcccjjj@users.noreply.github.com>
2021-01-10 15:01:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
 token=os.environ['PLUS_KEY']
-plant1=os.environ['PLANT1']
-fruit1=os.environ['FRUIT1']
-pet1=os.environ['PET1']
-dream_factory1=os.environ['DREAM_FACTORY1']
-jd_factory1=os.environ['JD_FACTORY1']
 def jd_subscribe(plant_code,fruit_code,pet_code,dream_factory_code,jd_factory_code):
     try:
         # 模拟浏览器打开网站
@@ -19,17 +14,14 @@ def jd_subscribe(plant_code,fruit_code,pet_code,dream_factory_code,jd_factory_co
         plant_code_url = "http://api.turinglabs.net/api/v1/jd/bean/create/"+str(plant_code)+"/"
         fruit_code_url = "http://api.turinglabs.net/api/v1/jd/farm/create/"+str(fruit_code)+"/"
         pet_code_url = "http://api.turinglabs.net/api/v1/jd/pet/create/"+str(pet_code)+"/"
-        dream_factory_url = "http://api.turinglabs.net/api/v1/jd/jxfactory/create/"+str(dream_factory_code)+"/"
-        jd_factory_url = "http://api.turinglabs.net/api/v1/jd/ddfactory/create/"+str(jd_factory_code)+"/"
+        dream_factory_url= "http://api.turinglabs.net/api/v1/jd/jxfactory/create/"+str(dream_factory_code)+"/"
         driver.get(plant_code_url)
         driver.implicitly_wait(100)
         driver.get(fruit_code_url)
         driver.implicitly_wait(100)
         driver.get(pet_code_url)
-        driver.implicitly_wait(100)
         driver.get(dream_factory_url)
         driver.implicitly_wait(100)
-        driver.get(jd_factory_url)
     except:
         driver.quit()
         print("上车失败!")
@@ -44,5 +36,30 @@ def jd_subscribe(plant_code,fruit_code,pet_code,dream_factory_code,jd_factory_co
         content ='上车成功' #改成你要的正文内容
         url = 'http://pushplus.hxtrip.com/customer/push/send?token='+token+'&title='+title+'&content='+content
         requests.get(url)
-if __name__ == "__main__":
-   jd_subscribe(plant1,fruit1,pet1,dream_factory1,jd_factory1)
+ #种豆得豆部分
+plant_code = os.environ['PLANT']  # 此时是字符串，记得一定要加引号啊。
+plant_code_num = int(plant_code.count('&'))+1
+print("你输入了%d个种豆得豆互助码" % plant_code_num)
+list1 = plant_code.split("&")
+
+#水果部分
+fruit_code = os.environ['FRUIT']
+fruit_code_num = int(fruit_code.count('&'))+1
+print("你提供了%d个水果互助码" % fruit_code_num)
+list2 = fruit_code.split("&")
+
+#东东萌宠部分
+pet_code = os.environ['PET']
+pet_code_num = int(pet_code.count('&'))+1
+print("你提供了%d个东东萌宠互助码" % pet_code_num)
+list3 = pet_code.split("&")
+
+#京喜工厂部分
+dream_factory_code = os.environ['DREAM_FACTORY']
+dream_factory_code_num = int(dream_factory_code.count('&'))+1
+print("你提供了%d个京喜工厂互助码" % dream_factory_code_num)
+list4 = dream_factory_code.split("&")
+ if __name__ == "__main__":
+    for i in range(fruit_code.count('&')+1):
+        jd_subscribe(list1[i],list2[i],list3[i],list4[i])
+
